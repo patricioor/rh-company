fe81c449ebb27a5bdb1adbb225aeb56d5f007d35
Tico <patricio.osterno1@gmail.com>
2024-11-07 09:45:21 -0300

    Primeiro commit

---
+++ src/main/java/io/github/patricioor/rh_company/RhCompanyApplication.java
@@ -0,0 +1,13 @@
+package io.github.patricioor.rh_company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/patricioor/rh_company/RhCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.patricioor.rh_company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
